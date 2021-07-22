fc5a49b4d64491fdddb2178f0a1c49a179fb604a
Chandra Shekhar <shekhar88085@gmail.com>
2021-07-22 23:07:56 +0530

    initial commit

---
+++ course_api/src/test/java/io/edar/brushup/CourseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.edar.brushup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

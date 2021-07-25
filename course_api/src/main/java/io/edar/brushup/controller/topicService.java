2224de2be7029754d670be53ec4645126ec9a365
Chandra Shekhar <shekhar88085@gmail.com>
2021-07-26 02:25:21 +0530

    replace classic methods body with crudRepository interface

--- course_api/src/main/java/io/edar/brushup/controller/topicService.java
+++ course_api/src/main/java/io/edar/brushup/controller/topicService.java
@@ -4,11 +4,15 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class topicService {
 
+	@Autowired
+	private topicsRepository topicsRepository;
+	
 	List<Topic> topics= new ArrayList<>( Arrays.asList(
 			
 			new Topic("Spring", "Spring Fra,ework", "S F Descriptiom"),
@@ -18,7 +22,11 @@ public class topicService {
 			));
 	
 	public List<Topic> getAllTopics(){
+		//return topics;
+		List<Topic> topics=new ArrayList<>();
+		topicsRepository.findAll().forEach(topics::add);
 		return topics;
+		
 	}
 	
 	public Topic getTopic(String id) {
@@ -28,22 +36,26 @@ public class topicService {
 	}
 	
 	public void addTopic(Topic topic) {
-		topics.add(topic);
+		//topics.add(topic);
+		topicsRepository.save(topic);
 	}
 
 	public void updateTopic(String id, Topic topic) {
 		// TODO Auto-generated method stub
-		for(int i=0;i<topics.size();i++) {
-			Topic t=topics.get(i);
-			if(t.getId().equals(id)) {
-				topics.set(i, topic);
-				return;
-			}
-		}
+//		for(int i=0;i<topics.size();i++) {
+//			Topic t=topics.get(i);
+//			if(t.getId().equals(id)) {
+//				topics.set(i, topic);
+//				return;
+//			}
+//		}
+		topicsRepository.save(topic);
 	}
 
 	public void deleteTopic(String id) {
-		topics.removeIf(t->t.getId().equals(id));
+//		topics.removeIf(t->t.getId().equals(id));
+		topicsRepository.deleteById(id);
+		
 	}
 	
 } 
